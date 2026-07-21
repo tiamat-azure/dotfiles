d0c490fd8333e1e8e9ec250c313106b9fef1c6c9
tiamat-azure <tiamat.azure@gmail.com>
2026-07-22 09:52:16 +1100

    feat: add wezterm multiplexer key table

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
+local act = wezterm.action
 
+-- Terminal
 config.default_prog = { "/usr/bin/zsh", "-l" }
 
+-- Apparence
 config.color_scheme = "rose-pine-moon"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
@@ -12,4 +14,112 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "NONE"
 config.enable_wayland = false
 
+-- Mode multiplexeur persistant
+config.key_tables = {
+  multiplex = {
+    -- Splits
+    {
+      key = "d",
+      action = act.SplitHorizontal({
+        domain = "CurrentPaneDomain",
+      }),
+    },
+    {
+      key = "s",
+      action = act.SplitVertical({
+        domain = "CurrentPaneDomain",
+      }),
+    },
+
+    -- Navigation
+    {
+      key = "LeftArrow",
+      action = act.ActivatePaneDirection("Left"),
+    },
+    {
+      key = "RightArrow",
+      action = act.ActivatePaneDirection("Right"),
+    },
+    {
+      key = "UpArrow",
+      action = act.ActivatePaneDirection("Up"),
+    },
+    {
+      key = "DownArrow",
+      action = act.ActivatePaneDirection("Down"),
+    },
+
+    -- Resize
+    {
+      key = "LeftArrow",
+      mods = "SHIFT",
+      action = act.AdjustPaneSize({ "Left", 3 }),
+    },
+    {
+      key = "RightArrow",
+      mods = "SHIFT",
+      action = act.AdjustPaneSize({ "Right", 3 }),
+    },
+    {
+      key = "UpArrow",
+      mods = "SHIFT",
+      action = act.AdjustPaneSize({ "Up", 3 }),
+    },
+    {
+      key = "DownArrow",
+      mods = "SHIFT",
+      action = act.AdjustPaneSize({ "Down", 3 }),
+    },
+
+    -- Panneaux
+    {
+      key = "z",
+      action = act.TogglePaneZoomState,
+    },
+    {
+      key = "w",
+      action = act.CloseCurrentPane({
+        confirm = true,
+      }),
+    },
+
+    -- Onglets
+    {
+      key = "c",
+      action = act.SpawnTab("CurrentPaneDomain"),
+    },
+    {
+      key = "x",
+      action = act.CloseCurrentTab({
+        confirm = true,
+      }),
+    },
+
+    -- Sortie mode multiplex
+    {
+      key = "Escape",
+      action = act.PopKeyTable,
+    },
+  },
+}
+
+-- Raccourcis globaux
+config.keys = {
+  {
+    key = "Space",
+    mods = "CTRL",
+    action = act.ActivateKeyTable({
+      name = "multiplex",
+      one_shot = false,
+    }),
+  },
+  {
+    key = "r",
+    mods = "CTRL|SHIFT",
+    action = act.ReloadConfiguration,
+  },
+}
+
 return config
+
+
